ee1582fa188ad724162d6dce853d62238bb00730
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-09 12:54:25 -0600

    update path

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/aplicacionEmpresarial'));
+app.use(express.static('./dist/Actividad-Gato'));
 app.get("/*",function(req, res){
-    res.sendFile("index.html",{root: './dist/aplicacionEmpresarial/'});
+    res.sendFile("index.html",{root: './dist/Actividad-Gato/'});
 });
 
 app.listen(process.env.PORT || 8080);
