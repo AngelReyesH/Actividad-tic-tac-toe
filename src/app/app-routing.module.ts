fd708a47d43c932e8e4bfd4cab6294450541ff87
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-09 12:47:14 -0600

    update server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "Juego-tic-tac-toe",
+    component: GameComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
